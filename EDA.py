60248eb6862fb21f04a0c59316501265f4c275e1
Cristian Valencia <crfvalenciam@gmail.com>
2023-05-30 10:56:08 +0200

    act

--- EDA.py
+++ EDA.py
@@ -53,50 +53,39 @@ st.set_page_config(
 )
 
 
-class BusinessIntelligence:
+class EDA:
     def __init__(self):
         self.df = None
         self.separator = ","
         self.bg_image = None
-        self.bg_image_url = (
-            "https://img.freepik.com/foto-gratis/fondo-verde-abstracto_1017-3218.jpg"
-        )
 
-    # def add_bg_from_local(self, image_file):
-    #     with open(image_file, "rb") as file:
-    #         encoded_string = base64.b64encode(file.read())
-    #     st.markdown(
-    #         f"""
-    #         <style>
-    #             .stApp {{
-    #             background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-    #             background-size: cover
-    #         }}
-    #         </style>
-    #         """,
-    #         unsafe_allow_html=True,
-    #     )
-
-    def add_bg_from_url(self, bg_image_url):
-        response = requests.get(bg_image_url)
-        bg_image = response.content
-        bg_image_base64 = base64.b64encode(bg_image).decode()
+    def add_bg_color(self):
         title_html = f"""
             <div style="text-align: center;">
-                <img src="https://upload.wikimedia.org/wikipedia/en/c/c9/Giant_logo.png" alt="Logo" style="width: 150px; height: auto;">
                 <h1 style="font-family: Helvetica Neue, serif;
                font-style: italic;
                font-weight: bold;
-               font-size: 35px;
+               font-size: 45px;
                color: white;
-               background-image: url(data:image/jpeg;base64,{bg_image_base64});
                background-repeat: no-repeat;
                background-size: cover;
-               padding: 20px;">Business Intelligence</h1>
+               padding: 20px;">Business Intelligence EDA</h1>
             </div>
 
         """
+
         st.markdown(title_html, unsafe_allow_html=True)
+
+        st.markdown(
+            """
+            <style>
+                .stApp {
+                    background-color: #1a1a1a; /* Azul oscuro mezclado con gris */
+                }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
 
     def load_file(_self):
         uploaded_file = st.file_uploader(
@@ -122,7 +111,7 @@ class BusinessIntelligence:
             options = {
                 "Contexto": "Contexto 📊",
                 "EDA": "EDA 🔍",
-                "Analisis Estadistico": "Analisis Estadistico 📈",
+                "Power-Bi": "Power-Bi 📈",
                 "Insights": "Insights ✅",
             }
 
@@ -469,9 +458,7 @@ class BusinessIntelligence:
 
         # Función principal para ejecutar el análisis de Business Intelligence
         # self.add_bg_from_local("fo.jpg")
-        self.add_bg_from_url(
-            "https://img.freepik.com/foto-gratis/fondo-verde-abstracto_1017-3218.jpg"
-        )
+        self.add_bg_color()
         self.load_file()
         self.visualize_data()
         # self.generate_insights()
@@ -480,7 +467,7 @@ class BusinessIntelligence:
 
 
 if __name__ == "__main__":
-    bi = BusinessIntelligence()
+    bi = EDA()
     url = (
         "https://www.giant.com.my/our-history/"  # La URL que deseas pasar a la función
     )
@@ -489,276 +476,3 @@ if __name__ == "__main__":
 
 # # en terminal poner dirección relativa de carpeta de report.py
 # # py -m streamlit run preprocesamiento.py
-
-# # Configurar página
-
-# # Establecer el tema personalizado
-
-# st.set_page_config(
-#     page_title="Business Intelligence",
-#     page_icon=":bar_chart:",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
-# )
-
-# st.set_option("deprecation.showPyplotGlobalUse", False)
-
-
-# @st.cache_resource
-# def cargar_datos():
-#     # Cargar datos costosos aquí
-#     datos = ...
-#     return datos
-
-
-# # Llamada a la función cargar_datos
-# datos = cargar_datos()
-
-
-# def add_bg_from_local(image_file):
-#     with open(image_file, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#     st.markdown(
-#         f"""
-#         <style>
-#             .stApp {{
-#             background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-#             background-size: cover
-#         }}
-#         </style>
-#         """,
-#         unsafe_allow_html=True,
-#     )
-
-
-# add_bg_from_local("fo.jpg")
-
-# # Obtener el contenido de la imagen de fondo desde la URL
-# bg_image_url = "https://img.freepik.com/foto-gratis/fondo-verde-abstracto_1017-3218.jpg"
-# response = requests.get(bg_image_url)
-# bg_image = response.content
-
-# # Convertir la imagen a base64
-# bg_image_base64 = base64.b64encode(bg_image).decode()
-
-# # Generar el código HTML con el título y el fondo de imagen
-# title_html = f"""
-#     <div style="background-image: url(data:image/jpeg;base64,{bg_image_base64});
-#                 background-repeat: no-repeat;
-#                 background-size: cover;
-#                 padding: -0px -0px;
-#                 text-align: center;">
-#         <img src="https://upload.wikimedia.org/wikipedia/en/c/c9/Giant_logo.png" alt="Logo" style="width: 190px; height: auto;">
-#         <h1 style="font-family: Helvetica Neue, serif;
-#                 font-style: italic;
-#                 font-weight: bold;
-#                 font-size: 35px;
-#                 color: white">Business Intelligence</h1>
-#     </div>
-# """
-
-# # Mostrar el título en Streamlit
-# st.markdown(title_html, unsafe_allow_html=True)
-
-# def load_data(file, separator):
-#     file_extension = file.name.split(".")[-1]
-
-#     # Cargar el archivo en un DataFrame según su extensión
-#     if file_extension == "xlsx":
-#         dataset = pd.read_excel(file)
-#     elif file_extension == "csv":
-#         if file.size == 0:
-#             dataset = None
-#         # Leer el archivo CSV con el separador seleccionado
-#         dataset = pd.read_csv(file, sep=separator)
-#     # elif file_extension == "xml":
-#     #     dataset = pd.read_xml(file)
-#     # elif file_extension == "no_xml":
-#     #     dataset = pd.read_csv(file)
-#     else:
-#         dataset = None
-
-#     return dataset
-
-
-# options = {
-#     "Contexto": "🌐 Contexto",
-#     "Análisis gráfico": "📊 Análisis gráfico",
-#     "Insights": "✅ Insights",
-#     "Modelos de predicción": "📝 Modelos de predicción",
-#     "Conclusiones": "📊 Conclusiones",
-# }
-
-# # Crear el sidebar desplegable
-# option = st.sidebar.selectbox("Selecciona una opción", list(options.keys()))
-
-# if option == "Contexto":
-#     st.markdown(
-#         "<style>body{background-color: #008000;}</style>", unsafe_allow_html=True
-#     )
-#     st.markdown(
-#         "<style>h1{color: #FFFFFF; font-family: Arial, sans-serif;}</style>",
-#         unsafe_allow_html=True,
-#     )
-#     st.markdown(
-#         "<style>p{color: #FFFFFF; font-family: Arial, sans-serif;}</style>",
-#         unsafe_allow_html=True,
-#     )
-
-#     titulo = "<h1 style='text-align: center; font-family: Arial, sans-serif; color: #88ff88; font-style: italic;'>Giant Supermarket</h1>"
-#     st.markdown(titulo, unsafe_allow_html=True)
-
-#     texto = """
-#     Giant es una cadena de supermercados fundada en 1944 en Kuala Lumpur. En 1974, abrieron el Teng Minimarket Centre (TMC) en Bangsar, lo que impulsó su expansión. En 1999, Dairy Farm International Holdings adquirió el 90% de la cadena, mientras que la familia Teng retuvo el resto. En 2003, la empresa matriz cambió su nombre a Dairy Farm Giant Retail Sdn Bhd. Actualmente, la compañía opera como subsidiaria de Dairy Farm International Holdings Limited y se le cambió el nombre a GCH Retail (Malasia) Sdn Bhd.
-
-#     GCH Retail es propiedad en un 30% de Syarikat Pesaka Antah Sdn Bhd, una empresa controlada por la familia real de Negeri Sembilan, y el 70% restante perteneceContinuación del código con la indentación corregida:a DFI Mauritius Ltd.
-
-#     En diciembre de 2011, Giant ingresó al mercado vietnamita, pero en febrero de 2018 vendieron su interés en una hipermercado en Vietnam a Auchan Retail Vietnam. Desde entonces, la compañía ha salido del negocio de supermercados e hipermercados en Vietnam.
-
-#     En los últimos años, Giant ha enfrentado dificultades en Malasia y Singapur debido a altos costos operativos, debilitamiento del gasto del consumidor y mayor competencia. Han cerrado varias tiendas en ambos países.
-
-#     En septiembre de 2020, Giant anunció una rebranding en Singapur, presentando un nuevo logo y características nuevas en sus tiendas. También redujeron los precios de los productos esenciales en un promedio del 20% durante seis meses. Esta rebranding se extendió a Malasia en abril de 2021, y se introdujo una sección llamada "Ringgit Zone" con productos a precios fijos de RM3 por artículo."""
-
-#     st.markdown(texto, unsafe_allow_html=True)
-
-# elif option == "Análisis gráfico":
-#     # Opción para cargar el archivo
-#     uploaded_file = st.file_uploader(
-#         "Cargar archivo procesado", type=["xlsx", "csv", "pickle", "parquet"]
-#     )
-
-#     if uploaded_file is not None:
-#         # Obtener el separador seleccionado si el archivo es de tipo CSV
-#         separator = ","
-#         if uploaded_file.type == "text/csv":
-#             dataset = pd.read_csv(uploaded_file, sep=separator)
-
-#         elif uploaded_file.type == "excel":
-#             dataset = pd.read_excel(uploaded_file)
-
-#         # Cargar el archivo pickle descargado
-#         uploaded_file = st.file_uploader(
-#             "Cargar archivo pickle descargado", type="pickle"
-#         )
-#         if uploaded_file is not None:
-#             dataset = pd.read_pickle(uploaded_file)
-
-#             # Verificar el tipo de dataset
-#             if isinstance(dataset, pd.DataFrame):
-#                 st.write("Archivo cargado exitosamente.")
-#                 # Resto de tu código en Streamlit utilizando el DataFrame cargado
-#             else:
-#                 st.write("Error: El archivo no contiene un DataFrame válido.")
-
-#         elif uploaded_file.name.endswith(".parquet"):
-#             dataset = pq.read_table(uploaded_file).to_pandas()
-
-#         df = dataset.copy()
-#         df.dtypes
-#         modify_data_types(df, categories_number=150)
-#         df.dtypes
-
-#         obj_cols = df.select_dtypes(include=["object"]).columns
-#         cat_cols = df.select_dtypes(include=["category"]).columns
-#         num_cols = df.select_dtypes(include=["int64", "float64"]).columns
-#         date_cols = df.select_dtypes(include=["datetime64[ns]"]).columns
-#         date_cols = date_cols.append(num_cols[-7:-1])
-#         num_cols = num_cols[0:-7]
-#         str_list = list(obj_cols) + list(cat_cols) + list(date_cols)
-#         cat_list = list(obj_cols) + list(cat_cols) + list(date_cols)
-#         asce_list = [False, True]
-
-#         col1, col2 = st.columns(2)
-
-#         # Contenido de la primera columna (col1)
-#         with col1:
-#             with st.sidebar:
-#                 main_column = st.selectbox(
-#                     "Seleccione la variable a consultar", str_list
-#                 )
-#                 main_num_col = st.selectbox("Seleccione la variable numerica", num_cols)
-#                 main_cat_col = st.selectbox(
-#                     "Seleccione la variable categorica", cat_cols
-#                 )
-#                 top = 10
-#                 orden = st.selectbox(
-#                     "Seleccione el orden del dataframe", list(range(1, 7))
-#                 )
-#                 ascen = st.selectbox("Descendente=False/Ascendente=True", [False, True])
-
-#                 size_title = st.selectbox(
-#                     "Tamaño del título", list(range(1, 6)), index=3
-#                 )
-#                 color = st.selectbox(
-#                     "Color del título",
-#                     ["Emrld", "Crimson", "Sapphire", "Gold", "Amethyst"],
-#                     index=0,
-#                 )
-#                 height = st.selectbox(
-#                     "Altura del gráfico", list(range(100, 1001, 100)), index=3
-#                 )
-#                 width = st.selectbox(
-#                     "Anchura del gráfico", list(range(500, 2001, 100)), index=5
-#                 )
-#                 ori = st.selectbox(
-#                     "Orientación del gráfico", ["Vertical", "Horizontal"], index=0
-#                 )
-
-#                 if ori == "Vertical":
-#                     ori = "v"
-#                 else:
-#                     ori = "h"
-
-#             df_top_c = top_df(df, main_column, main_num_col, main_cat_col, top, ascen)
-
-#             title = f"<h3 style='text-align: center; font-family: Arial, sans-serif;'><b>{top} {main_column} with most {main_num_col}</b></h3>"
-#             st.markdown(title, unsafe_allow_html=True)
-
-#             fig = barv_plotly(
-#                 df_top_c,
-#                 main_column,
-#                 main_num_col,
-#                 orden,
-#                 size_title,
-#                 color,
-#                 height,
-#                 width,
-#                 ori,
-#             )
-#             st.plotly_chart(fig)
-
-#         # Contenido de la segunda columna (col2)
-#         with col2:
-#             fig = barv_plotly(
-#                 df_top_c,
-#                 main_column,
-#                 main_num_col,
-#                 orden,
-#                 size_title,
-#                 color,
-#                 height,
-#                 width,
-#                 ori,
-#             )
-#             st.plotly_chart(fig)
-
-# elif option == "Insights":
-#     st.code(
-#         """
-#         # Código para los insights
-#         """
-#     )
-
-# elif option == "Modelos de predicción":
-#     st.code(
-#         """
-#         # Código para los modelos de predicción
-#         """
-#     )
-
-# elif option == "Conclusiones":
-#     st.code(
-#         """
-#         # Código para las conclusiones
-#         """
-#     )
