f2368932ea448b5def0b713d6e0733f58ff6512e
Cristian Valencia <crfvalenciam@gmail.com>
2023-06-16 11:11:22 +0200

    act

--- EDA.py
+++ EDA.py
@@ -318,10 +318,10 @@ class EDA:
                     self.ascen,
                 )
                 self.df_top_l = self.df_top_l.sort_values(
-                    self.df_top_l.columns[1], ascending=False
+                    self.df_top_l.columns[0], ascending=False
                 )
 
-                st.dataframe(self.df_top_l)
+                # st.dataframe(self.df_top_l)
 
                 self.df_filtered_top_date = self.df_top_l[
                     (self.df_top_l[self.df_top_l.columns[0]] >= self.fecha_min)
